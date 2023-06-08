e16102fb738eaf117e1db0885d189d61420d0141
Jae <jaeladu1@gmail.com>
2023-06-08 12:44:17 -0300

    Fix database initialization

--- back/database.ts
+++ back/database.ts
@@ -1,18 +1,22 @@
 import { credential, database, firestore } from "firebase-admin";
 import { applicationDefault, initializeApp } from "firebase-admin/app";
-// const key = require("./firebaseKey.json"); //line needed if importing from json file key
 
-//environment saved key
-const app = initializeApp({
-   credential: applicationDefault(),
-   databaseURL: "https://dwf-m6-desafio-f0916-default-rtdb.firebaseio.com/",
-});
-
-//imported from json file key
-// const app = initializeApp({
-//    credential: credential.cert(key),
-//    databaseURL: "https://dwf-m6-desafio-f0916-default-rtdb.firebaseio.com/",
-// });
+const environment = process.env.ENVIRONMENT;
+if (environment == "development") {
+   //environment saved key
+   const app = initializeApp({
+      credential: applicationDefault(),
+      databaseURL: "https://dwf-m6-desafio-f0916-default-rtdb.firebaseio.com/",
+   });
+}
+if (environment == "Production") {
+   const key = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+   //imported from json file key
+   const app = initializeApp({
+      credential: credential.cert(key),
+      databaseURL: "https://dwf-m6-desafio-f0916-default-rtdb.firebaseio.com/",
+   });
+}
 
 const firebaseDatabase = database();
 const firestoreDatabase = firestore();
